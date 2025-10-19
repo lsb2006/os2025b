322a10ed3b214a394d33887e6a070ce382c28f8d
leesoonbin060913 <leesoonbin060913@gmail.com>
2025-10-19 23:41:44 +0900

    copy

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This repository is for the Open Source Programming couse in the Fall 2025 semester.
+This repository is for the Open Source Programming course in the Fall 2025 semester.
--- week06/types/main.go
+++ week06/types/main.go
@@ -7,8 +7,7 @@ import (
 
 func main() {
 	// var 64f float64
-	// fmt.Println(64f, reflect.TypeOf(f64))
-
+	// fmt.Println(64f, reflect.TypeOf(64f))
 
 	totalPrice := 1000
 	fmt.Println(totalPrice, reflect.TypeOf(totalPrice))
